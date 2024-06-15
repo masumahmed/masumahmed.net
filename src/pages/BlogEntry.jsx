cc9f9cc393aceb68e24e07740d827af02188f7ef
Masum Ahmed <MasumAhmed1798@gmail.com>
2024-06-15 00:40:07 -0400

    preview

--- src/pages/BlogEntry.jsx
+++ src/pages/BlogEntry.jsx
@@ -11,7 +11,7 @@ function BlogEntry() {
     const [htmlContent, setHtmlContent] = useState('');
     let { hash } = useParams();
     
-    let date = '', author = '', md="", description = '', title = '';
+    let date = '', author = '', md="", description = '', title = '', img = '';
     BlogData.forEach(entry => {            
         if (entry.hash === hash) {
             title = entry.title;
@@ -19,6 +19,7 @@ function BlogEntry() {
             author = entry.author;
             md = entry.md;
             description = entry.description;
+            img = entry.img;
         }
     });
     
@@ -34,21 +35,17 @@ function BlogEntry() {
         setHtmlContent(html);
     }, []);
 
-
-    title = title.replace(/\b\w/g, l => l.toUpperCase());
-    title = title.replace('-', ' ');
-
     return <>
         <div id="bodyWrapper">
             <Header />
             <div className="content">
                 <div className="con border markdown" style={{ "borderBottom": "1px solid #3a3a3a" }}>
-                    <br />
                     {!date && <>
                         <h1 style={{ "textAlign": "center" }}>404</h1>
                         <h1 style={{ "textAlign": "center" }}>no such blog exists</h1>
                     </>}
                     {date && <>
+                        <img className="previewImage" src={img} />
                         <h1 style={{ "textAlign": "left" }}>{title}</h1>
                         <hr />
                         <p><i>{author} - {date} - {description}</i></p>
