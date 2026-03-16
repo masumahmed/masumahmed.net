76cbd2ccd06a371d4a8cfce814698d4eab247bbe
Masum Ahmed <masumahmed1798@gmail.com>
2026-03-15 23:35:53 -0400

    photos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Intro from '../data/IntroData.json';
 
 function Home() {
     const [text, setText] = useState("Software Engineer");
-    const [speed, setSpeed] = useState(700);
+    const [speed, setSpeed] = useState(800);
 
     useEffect(() => {        
         const labels = Intro[0].text.split(' ');
@@ -31,7 +31,8 @@ function Home() {
             </div>
             <div className="content nuetral">
                 <div className="con">
-                    <div id="readme"><h1 id="hello">Hello</h1>
+                    <div id="readme">
+                        <h1 id="hello">Hello</h1>
                         <p>Welcome to my personal website. This site is intended to showcase my porfolio, projects and notes. It's also a handy way to get in contact with me and serves as a central hub for my professional online appearance.</p>
                         <p>My name is <code>Masum Ahmed</code> and I am a computer science major with a passion for programming and web development. I am currently studying at CUNY Hunter College and have a strong interest in web development and data science.</p>
                         <p>In my free time, I enjoy learning about new technologies and exploring the latest trends in the field of computer science. I have experience with a variety of programming languages, including <code>C++</code>, <code>Python</code>, and <code>JavaScript</code>, and I am always looking for opportunities to apply my knowledge to real-world problems.</p>
