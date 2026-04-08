bb049740867573ea955ae6b9169d8247856e25cb
Masum Ahmed <masumahmed1798@gmail.com>
2026-04-07 20:08:55 -0400

    2026

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'masumahmed.net',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
