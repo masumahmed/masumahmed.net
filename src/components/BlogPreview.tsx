53d94bf23cac4e9c08ddd90c346a6ff286255b80
Masum Ahmed <masumahmed1798@gmail.com>
2026-06-20 17:02:43 -0400

    code

--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -1,35 +1,42 @@
-import '../styles/BlogPreview.scss';
+import "../styles/BlogPreview.scss";
+import placeholder from "../assets/code.svg";
 import { Link } from "react-router-dom";
 
 function BlogPreview({ props }: any) {
-    let { hash, title, date, tags, description, img } = props;
-    let titleURL = title.replace(/ /g, '-').toLowerCase();
-    if (description.length > 200)
-        description = description.substring(0, 200) + '...';
+  let { hash, title, date, tags, description, img } = props;
+  let titleURL = title.replace(/ /g, "-").toLowerCase();
+  if (description.length > 200)
+    description = description.substring(0, 200) + "...";
 
-    // remove tags leading spaces and prepend '#'
-    tags = tags.replace(/ /g, '');
-    tags = tags.split(',').map((tag: string) => {
-        return '#' + tag;
-    }).join(', ');
+  // remove tags leading spaces and prepend '#'
+  tags = tags.replace(/ /g, "");
+  tags = tags
+    .split(",")
+    .map((tag: string) => {
+      return "#" + tag;
+    })
+    .join(", ");
 
-    if (img === "")
-        img = "/assets/img/code-circle.svg";
-
-    return <>
-        <div className="preview" style={{ "borderBottom": "1px solid #3a3a3a" }}>
-            <div className='left'>
-                <Link to={`/blog/${hash}/${titleURL}`} >
-                    <img src={img} />
-                </Link>
-            </div>
-            <div className='right'>
-                <h1><Link to={`/blog/${hash}/${titleURL}`} >{title}</Link></h1>
-                <p>{description}</p>
-                <p>{date} • {tags}</p>
-            </div>
+  return (
+    <>
+      <div className="preview" style={{ borderBottom: "1px solid #3a3a3a" }}>
+        <div className="left">
+          <Link to={`/blog/${hash}/${titleURL}`}>
+            <img src={img == "" ? placeholder : img} />
+          </Link>
         </div>
+        <div className="right">
+          <h1>
+            <Link to={`/blog/${hash}/${titleURL}`}>{title}</Link>
+          </h1>
+          <p>{description}</p>
+          <p>
+            {date} • {tags}
+          </p>
+        </div>
+      </div>
     </>
+  );
 }
 
 export default BlogPreview;
